bd3a2441867b76e8d154bd51871f1db0c75f1988
Glitch (glitch-hello-website) <none>
2022-06-03 10:14:53 +0000

    🏌🐷 Checkpoint ./script.js:45217956/14225 ./style.css:45217956/17

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function RemoveMe(object) {
 
 function addTableRow(object){
   var addRowAppend =
-      "<tr><td><input type='button' id='addRow' value='+' onClick='addTableRow(this)'/></td><td id='currentYear'>Current year: </td><td><input type='text' value='' required/></td><td><input type='text' value='' required/></td><td><input type='text' value='' required/></td><td><input type='text' value='N/A' required/></td><td><input type='button' id='deleteRow' value='🗑️' onclick='RemoveMe(this)'/></td></tr>";
+      "<tr><td><input type='button' id='addRow' value='+' onClick='addTableRow(this)'/></td><td id='currentYear'></td>Current year: <td contenteditable></td><td contenteditable></td><td contenteditable></td><td>N/A</td><td><input type='button' id='deleteRow' value='🗑️' onclick='RemoveMe(this)'/></td></tr>";
     $(object).parents('tr').after(addRowAppend);
   tableRowSize++;
   console.log("add row function is running");
@@ -93,4 +93,8 @@ $(document).ready(function () {
       return true;
     }
   );
+});
+
+$( document ).ready(function(){
+$("#form-table td:empty").text("Was empty").css('background', 'rgb(255,220,200)');
 });
