b51e97232df4950e21fc6420f9f7fa9edcaf5673
Glitch (glitch-hello-website) <none>
2022-06-03 10:03:26 +0000

    🌦🎅 Checkpoint ./script.js:45217956/21453

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$("#myform").submit(function () {
-  $("#form-table").show();
+$('#myform').submit(function() {
+  $('#form-table').show();
   return false;
 });
 
-let tableRowSize = 0;
+let tableRowSize=0;
 $("#businessYear").datepicker({
   format: "yyyy",
   viewMode: "years",
@@ -16,37 +16,35 @@ function showDownloadButton() {
 }
 
 function ExportToExcel(type, fn, dl) {
-  var elt = document.getElementById("form-table");
-  var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
-  return dl
-    ? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
-    : XLSX.writeFile(wb, fn || "MySheetName." + (type || "xlsx"));
-}
+       var elt = document.getElementById('form-table');
+       var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+       return dl ?
+         XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
+         XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
+    }
 
 function RemoveMe(object) {
-  $(object).parents("tr").remove();
+  $(object).parents('tr').remove();
   renumberRows();
-  tableRowSize--;
-  console.log("tableRowSize on delete: " + tableRowSize);
+    tableRowSize--;
+    console.log("tableRowSize on delete: "+tableRowSize);
 }
 
-function addTableRow(object) {
+function addTableRow(object){
   var addRowAppend =
-    "<tr><td><input type='button' id='addRow' value='+' onClick='addTableRow(this)'/></td><td id='currentYear'></td>Current year: <td><input type='text' value='' required/></td><td><input type='text' value='' required/></td><td><input type='text' value='' required/></td><td><input type='text' value='N/A' required/></td><td><input type='button' id='deleteRow' value='🗑️' onclick='RemoveMe(this)'/></td></tr>";
-  $(object).parents("tr").after(addRowAppend);
+      "<tr><td><input type='button' id='addRow' value='+' onClick='addTableRow(this)'/></td><td id='currentYear'>Current year: </td><td><input type='text' value='' required/></td><td><input type='text' value='' required/></td><td><input type='text' value='' required/></td><td><input type='text' value='N/A' required/></td><td><input type='button' id='deleteRow' value='🗑️' onclick='RemoveMe(this)'/></td></tr>";
+    $(object).parents('tr').after(addRowAppend);
   tableRowSize++;
   console.log("add row function is running");
-  console.log("tableRowSize on add: " + tableRowSize);
+  console.log("tableRowSize on add: "+tableRowSize);
   renumberRows();
 }
 
 function renumberRows() {
-  $("table#form-table tbody tr").each(function (index) {
-    $(this)
-      .children("#currentYear")
-      .text("Current Year: " + (index + 1));
-    console.log("renumber is running!");
-  });
+    $('table#form-table tbody tr').each(function(index) {
+        $(this).children('#currentYear').text("Current Year: "+ (index+1) );
+        console.log("renumber is running!");
+    });
 }
 
 $(document).ready(function () {
@@ -76,22 +74,12 @@ $(document).ready(function () {
         for (var i = 1; i <= rowFinalNum + 1; i++) {
           var businessYearLoop = year;
           tableRowSize++;
-          console.log("tableRowSize: " + tableRowSize);
+          console.log("tableRowSize: "+tableRowSize);
           console.log(
             businessYearEnd + "/" + businessYear + " " + itf + " " + legalEntity
           );
           resultHtml +=
-            "<tr id='table' ><td><input type='button' id='addRow' value='+' onClick='addTableRow(this)'/></td><td id='currentYear'>Current year: " +
-            (i - 1) +
-            "</td><td contenteditable>" +
-            businessYearEnd +
-            "/" +
-            year +
-            "</td><td><input type='text' value='" +
-            itf +
-            "' required></td><td><input type='text' value='" +
-            legalEntity +
-            "' required/></td><td><input type='text' value='N/A' required/></td><td><input type='button' id='deleteRow' value='🗑️'onclick='RemoveMe(this)'/></td></tr>";
+            "<tr id='table' ><td><input type='button' id='addRow' value='+' onClick='addTableRow(this)'/></td><td id='currentYear'>Current year: "+(i-1)+"</td><td contenteditable>"+businessYearEnd+"/"+year+"</td><td contenteditable>"+itf+"</td><td contenteditable>"+legalEntity+"</td><td contenteditable>N/A</td><td><input type='button' id='deleteRow' value='🗑️'onclick='RemoveMe(this)'/></td></tr>"
           year--;
           console.log("inside table building loop");
         }
